a286e3bbb704a1c1920a076706fe587020dbce4f
Petteri Suckman <petteri.suckman@gmail.com>
2026-02-13 16:11:13 +0200

    add some nice ~ to the side, like in Vim. Center the title better.

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,64 @@ use crossterm::{
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
     terminal,
 };
+use ropey;
 use std::io::{self, Write};
+use crossterm::style::{Attribute, SetAttribute};
 
 const VERSION: &str = "0.0.1";
 
 fn print_editor_version(stdout: &mut io::Stdout) {
     let (cols, rows) = terminal::size().unwrap();
     let title = format!("EMED editor version {}", VERSION);
+    let chars = title.chars().count();
     let _ = execute!(
         stdout,
-        cursor::MoveTo((cols / 2) - 20, (rows / 2) - 5),
-        Print(&title)
+        SetBackgroundColor(Color::Black),
+        SetForegroundColor(Color::Magenta),
+        SetAttribute(Attribute::Bold),
+        cursor::MoveTo((cols / 2) - chars as u16/2, rows / 2 - 2 ),
+        Print(&title),
+        cursor::Hide
     );
 }
 
-fn set_colour_scheme(stdout: &mut io::Stdout) {
+fn initialise_screen(stdout: &mut io::Stdout) {
+    let (_, rows) = terminal::size().unwrap();
+    let x = 0;
+
+    for y in 0..rows {
+        let _ = execute!(stdout, cursor::MoveTo(x, y), Print("~\n"));
+    }
+}
+
+fn _set_colour_scheme(stdout: &mut io::Stdout) {
     let _ = execute!(
         stdout,
         terminal::Clear(terminal::ClearType::All),
-        SetBackgroundColor(Color::Red),
-        SetForegroundColor(Color::Blue)
+        SetBackgroundColor(Color::Black),
+        SetForegroundColor(Color::Magenta)
     );
 }
 
+fn clear_screen(stdout: &mut io::Stdout) -> io::Result<()> {
+    execute!(
+        stdout,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::MoveTo(0, 0)
+    )?;
+    Ok(())
+}
+
 fn main() -> io::Result<()> {
-    //let version = 0.01.to_string();
 
     let mut stdout = io::stdout();
     let _ = terminal::enable_raw_mode();
+    clear_screen(&mut stdout)?;
 
-    set_colour_scheme(&mut stdout);
     print_editor_version(&mut stdout);
+//    set_colour_scheme(&mut stdout);
+
+    initialise_screen(&mut stdout);
 
     let mut saw_ctrl_x = false;
     loop {
